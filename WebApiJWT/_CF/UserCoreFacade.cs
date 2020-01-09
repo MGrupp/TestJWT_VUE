2835676a886d23e1a5084b680c48ee83b61ee82d
MGrupp <55598886+MGrupp@users.noreply.github.com>
2020-01-09 07:00:46 -0500

    Add files via upload

---
+++ WebApiJWT/_CF/UserCoreFacade.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using WebApiJWT;
+using WebApiJWT.Models;
+
+namespace WebApiJWT
+{
+    public partial class CoreFacade : ICoreFacade
+    {
+        public UserCO getUserCO()
+        {
+            if (this.controllersInstances.Contains("UserCO"))
+                return (UserCO)this.controllersInstances["UserCO"];
+            else
+            {
+                UserCO UserCo = new UserCO();
+                this.controllersInstances.Add("UserCO", UserCo);
+                return UserCo;
+            }
+        }
+
+        public BusinessResult CreateUser(BOUser objUser)
+        {
+            return this.getUserCO().CreateUser(objUser);
+        }
+
+        public BusinessResult UpdateUser(BOUser objUser)
+        {
+            return this.getUserCO().UpdateUser(objUser);
+        }
+
+        public BusinessResult DeleteUser(int id)
+        {
+            return this.getUserCO().DeleteUser(id);
+        }
+
+        public BusinessResult GetUserById(int id)
+        {
+            return this.getUserCO().GetUserById(id);
+        }
+
+        public List<BOUser> GetAllUsers()
+        {
+            return this.getUserCO().GetAllUsers();
+        }
+
+        public BusinessResult UserLogin(BOUser objUser)
+        {
+            return this.getUserCO().UserLogin(objUser);
+        }
+    }
+}
